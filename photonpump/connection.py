82159feda345fb650da13eabccc6a35dc7c81085
Bob Gregory <bob@made.com>
2018-01-04 14:20:20 +0000

    Crappy volatile subscriptions

--- photonpump/connection.py
+++ photonpump/connection.py
@@ -2,11 +2,12 @@ import asyncio
 import logging
 import random
 import struct
-from typing import Dict, Sequence
 import uuid
+from typing import Dict, Sequence
 from uuid import UUID
-from .messages import *
+
 from .exceptions import *
+from .messages import *
 
 __version__ = '0.1.0'
 
@@ -24,7 +25,8 @@ class Event(list):
             f(*args, **kwargs)
 
     def __repr__(self):
-        return "Event(%)" % list.__repr__(self)
+        return "Event(%s)" % list.__repr__(self)
+
 
 class ConnectionHandler:
 
@@ -57,11 +59,14 @@ class ConnectionHandler:
         await self.queue.put(ConnectionHandler.OK)
 
     def run(self, host, port):
-        self._run_loop = asyncio.ensure_future(self._run(host, port), loop=self._loop)
+        self._run_loop = asyncio.ensure_future(
+            self._run(host, port), loop=self._loop
+        )
 
     async def _run(self, host, port):
         while (True):
             msg = await self.queue.get()
+
             if (msg == ConnectionHandler.OK):
                 self._logger.debug("Eventstore connection is OK")
                 self._current_attempts = 0
@@ -69,14 +74,17 @@ class ConnectionHandler:
                 await self._attempt_connect(host, port)
             elif msg == ConnectionHandler.HANGUP:
                 self._run_loop.cancel()
-                return
 
+                return
 
             self._last_message = msg
 
     async def _attempt_connect(self, host, port):
         self._current_attempts += 1
-        self._logger.debug("Attempting connection to %s:%d attempt %d of %d", host, port, self._current_attempts, self._max_attempts)
+        self._logger.debug(
+            "Attempting connection to %s:%d attempt %d of %d", host, port,
+            self._current_attempts, self._max_attempts
+        )
 
         if self._last_message == ConnectionHandler.CONNECT:
             await asyncio.sleep(random.uniform(0, self._current_attempts))
@@ -84,6 +92,7 @@ class ConnectionHandler:
             await self._loop.create_connection(self.getProtocol, host, port)
         except Exception as e:
             self._logger.error(e)
+
             if self._current_attempts == self._max_attempts:
                 raise
             await self._attempt_connect(host, port)
@@ -92,7 +101,7 @@ class ConnectionHandler:
         return self._protocol
 
 
-class EventstoreProtocol (asyncio.streams.FlowControlMixin):
+class EventstoreProtocol(asyncio.streams.FlowControlMixin):
 
     _length = struct.Struct('<I')
     _head = struct.Struct('>BBQQ')
@@ -120,25 +129,36 @@ class EventstoreProtocol (asyncio.streams.FlowControlMixin):
         self._running = True
         self._is_connected = True
         self.is_connecting = False
-        self._logger.info('PhotonPump is connected to eventstore instance at ' + str(transport.get_extra_info('peername', 'ERROR')))
+        self._logger.info(
+            'PhotonPump is connected to eventstore instance at ' +
+            str(transport.get_extra_info('peername', 'ERROR'))
+        )
 
         self._reader = asyncio.StreamReader(loop=self._loop)
         self._reader.set_transport(transport)
 
-        self._writer = asyncio.StreamWriter(self._transport, self, None, self._loop)
+        self._writer = asyncio.StreamWriter(
+            self._transport, self, None, self._loop
+        )
 
-        self._read_loop = asyncio.ensure_future(self._read_responses(), loop=self._loop)
-        self._write_loop = asyncio.ensure_future(self._process_queue(), loop=self._loop)
-
+        self._read_loop = asyncio.ensure_future(
+            self._read_responses(), loop=self._loop
+        )
+        self._write_loop = asyncio.ensure_future(
+            self._process_queue(), loop=self._loop
+        )
 
     def eof_received(self):
-        self._logger.log(logging.INFO, 'EOF received in EventstoreProtocol, closing connection')
+        self._logger.log(
+            logging.INFO,
+            'EOF received in EventstoreProtocol, closing connection'
+        )
+
         if self._is_connected:
             self.close()
+
         if not self.is_connecting:
             asyncio.ensure_future(self.connect(), loop=self._loop)
-
-
 
     def data_received(self, data):
         """ Process data received from Eventstore.  """
@@ -151,6 +171,7 @@ class EventstoreProtocol (asyncio.streams.FlowControlMixin):
         else:
             self._logger.error("Lost connection to Eventstore %s" % exc)
             self.close()
+
             if not self.is_connecting:
                 asyncio.ensure_future(self.connect(), loop=self._loop)
 
@@ -172,15 +193,20 @@ class EventstoreProtocol (asyncio.streams.FlowControlMixin):
     async def connect(self):
         await self._connectHandler.connect()
 
-
     async def _process_queue(self):
         if self.next:
             self.next.send(self._writer)
 
         while self._is_connected:
             self.next = await self._queue.get()
-            self._logger.debug(self.next)
-            self.next.send(self._writer)
+            self._logger.debug("Got a message to send!")
+            try:
+                self.next.send(self._writer)
+            except Exception as e:
+                self._logger.error(
+                    "Failed to send message %s", e, exc_info=True
+                )
+            self._logger.debug("Did that")
             try:
                 await self._writer.drain()
             except Exception as e:
@@ -190,10 +216,11 @@ class EventstoreProtocol (asyncio.streams.FlowControlMixin):
         """Read a message header from the StreamReader."""
         next_msg_len = await self._reader.read(SIZE_UINT_32)
         next_header = await self._reader.read(HEADER_LENGTH)
-        (cmd, flags,a,b) = self._head.unpack(next_header)
-        (size,) = self._length.unpack(next_msg_len)
+        (cmd, flags, a, b) = self._head.unpack(next_header)
+        (size, ) = self._length.unpack(next_msg_len)
         self._logger.debug("Next message len is %d", size)
-        id = uuid.UUID(int=(a << 64 | b ))
+        id = uuid.UUID(int=(a << 64 | b))
+
         return Header(size, cmd, flags, id)
 
     async def read_message(self):
@@ -201,42 +228,52 @@ class EventstoreProtocol (asyncio.streams.FlowControlMixin):
         bytes_remaining = header.size - HEADER_LENGTH
         next_chunk = await self._reader.read(bytes_remaining)
         bytes_read = len(next_chunk)
+
         if bytes_read == bytes_remaining:
             return header, next_chunk
         else:
             body = bytearray(next_chunk)
+
             while bytes_remaining > 0:
                 bytes_remaining -= bytes_read
                 next_chunk = await self._reader.read(bytes_remaining)
                 bytes_read = len(next_chunk)
                 body.extend(next_chunk)
-            return header, body
 
+            return header, body
 
     async def _read_responses(self):
         """Loop forever reading messages and invoking the operation that caused them"""
+
         while True:
             header, data = await self.read_message()
             await self._connectHandler.ok()
+            self._logger.debug("Received message %s", header)
 
-            if header.cmd  == TcpCommand.HeartbeatRequest:
+            if header.cmd == TcpCommand.HeartbeatRequest:
+                self._logger.debug("Responding to heartbeat")
                 await self.enqueue(HeartbeatResponse(header.correlation_id))
+
                 continue
 
             operation = self._pending_operations[header.correlation_id]
             self._logger.debug("Received response to operation %s", operation)
             await operation.handle_response(header, data, self)
-            del self._pending_operations[header.correlation_id]
 
+            if operation.is_complete:
+                del self._pending_operations[header.correlation_id]
 
     def close(self, hangup=False):
         """Close the underlying StreamWriter and cancel pending Operations."""
         self.running = False
+
         if hangup:
             self._connectHandler.hangup()
+
         for _, op in self._pending_operations.items():
             op.cancel()
-        if(self._read_loop):
+
+        if (self._read_loop):
             self._read_loop.cancel()
             self._write_loop.cancel()
             self._transport.close()
@@ -259,20 +296,23 @@ class Connection:
         self.loop = loop
         self.operations = {}
         self.queue = asyncio.Queue(maxsize=100)
-        self.protocol = EventstoreProtocol(host, port, self.queue, self.operations, loop=self.loop)
+        self.protocol = EventstoreProtocol(
+            host, port, self.queue, self.operations, loop=self.loop
+        )
 
     async def connect(self):
-       await self.protocol.connect()
-       self.connected()
+        await self.protocol.connect()
+        self.connected()
 
     def close(self):
         self.protocol.close(True)
         self.disconnected()
 
-    async def ping(self, correlation_id: UUID=None):
+    async def ping(self, correlation_id: UUID = None):
         correlation_id = correlation_id
         cmd = Ping(correlation_id=correlation_id, loop=self.loop)
         await self.protocol.enqueue(cmd)
+
         return await cmd.future
 
     async def publish_event(
@@ -283,10 +323,17 @@ class Connection:
             id=uuid.uuid4(),
             metadata=None,
             expected_version=-2,
-            require_master=False):
+            require_master=False
+    ):
         event = NewEvent(type, id, body, metadata)
-        cmd = WriteEvents(stream, [event], expected_version=expected_version, require_master=require_master, loop=self.loop)
+        cmd = WriteEvents(
+            stream, [event],
+            expected_version=expected_version,
+            require_master=require_master,
+            loop=self.loop
+        )
         await self.protocol.enqueue(cmd)
+
         return await cmd.future
 
     async def publish(
@@ -294,9 +341,17 @@ class Connection:
             stream: str,
             events: Sequence[NewEventData],
             expected_version=ExpectedVersion.Any,
-            require_master=False):
-        cmd = WriteEvents(stream, events, expected_version=expected_version, require_master=require_master, loop=self.loop)
+            require_master=False
+    ):
+        cmd = WriteEvents(
+            stream,
+            events,
+            expected_version=expected_version,
+            require_master=require_master,
+            loop=self.loop
+        )
         await self.protocol.enqueue(cmd)
+
         return await cmd.future
 
     async def get_event(
@@ -304,40 +359,64 @@ class Connection:
             stream: str,
             resolve_links=True,
             require_master=False,
-            correlation_id: UUID=None):
+            correlation_id: UUID = None
+    ):
         correlation_id = correlation_id
         cmd = ReadEvent(stream, resolve_links, require_master, loop=self.loop)
         await self.protocol.enqueue(cmd)
+
         return await cmd.future
 
     async def get(
             self,
             stream: str,
-            direction: StreamDirection=StreamDirection.Forward,
-            from_event: int=0,
-            max_count: int=100,
-            resolve_links: bool=True,
-            require_master: bool=False,
-            correlation_id: UUID=None):
+            direction: StreamDirection = StreamDirection.Forward,
+            from_event: int = 0,
+            max_count: int = 100,
+            resolve_links: bool = True,
+            require_master: bool = False,
+            correlation_id: UUID = None
+    ):
         correlation_id = correlation_id
-        cmd = ReadStreamEvents(stream, from_event, max_count, resolve_links, require_master, loop=self.loop)
+        cmd = ReadStreamEvents(
+            stream,
+            from_event,
+            max_count,
+            resolve_links,
+            require_master,
+            loop=self.loop
+        )
         await self.protocol.enqueue(cmd)
+
         return await cmd.future
 
     async def iter(
             self,
             stream: str,
-            direction: StreamDirection=StreamDirection.Forward,
-            from_event: int=0,
-            batch_size: int=100,
-            resolve_links: bool=True,
-            require_master: bool=False,
-            correlation_id: UUID=None):
+            direction: StreamDirection = StreamDirection.Forward,
+            from_event: int = 0,
+            batch_size: int = 100,
+            resolve_links: bool = True,
+            require_master: bool = False,
+            correlation_id: UUID = None
+    ):
         correlation_id = correlation_id
-        cmd = IterStreamEvents(stream, from_event, batch_size, resolve_links, require_master, loop=self.loop)
+        cmd = IterStreamEvents(
+            stream,
+            from_event,
+            batch_size,
+            resolve_links,
+            require_master,
+            loop=self.loop
+        )
         await self.protocol.enqueue(cmd)
         async for e in cmd.iterator:
             yield e
+
+    async def subscribe(self, stream: str, volatile=False):
+        cmd = CreateVolatileSubscription(stream, loop=self.loop)
+        await self.protocol.enqueue(cmd)
+        return await cmd.future
 
 
 class ConnectionContextManager:
@@ -347,6 +426,7 @@ class ConnectionContextManager:
 
     async def __aenter__(self):
         await self.conn.connect()
+
         return self.conn
 
     async def __aexit__(self, exc_type, exc, tb):
