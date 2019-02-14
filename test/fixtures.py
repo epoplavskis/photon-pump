017166d696608f9e552a2188480a518c33ce75de
Brandon <brandon2255p@gmail.com>
2019-02-14 23:45:30 +0200

    Added fixture for creating two streams

--- test/fixtures.py
+++ test/fixtures.py
@@ -19,3 +19,33 @@ async def given_a_stream_with_three_events(c, stream_name):
             ),
         ],
     )
+
+async def given_two_streams_with_two_events(c):
+    await c.publish(
+        "stream_one",
+        [
+            messages.NewEvent(
+                "pony_jumped",
+                data={"Pony": "Derpy Hooves", "Height": 10, "Distance": 13},
+            ),
+            messages.NewEvent(
+                "pony_jumped",
+                data={"Pony": "Sparkly Hooves", "Height": 4, "Distance": 9},
+            ),
+            
+        ],
+    )
+    await c.publish(
+        "stream_two",
+        [
+            messages.NewEvent(
+                "pony_jumped",
+                data={"Pony": "Unlikely Hooves", "Height": 63, "Distance": 23},
+            ),
+            messages.NewEvent(
+                "pony_jumped",
+                data={"Pony": "Glitter Hooves", "Height": 11, "Distance": 94},
+            ),
+            
+        ],
+    )
