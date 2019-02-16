a6c8d4d2dd166d9d919f9dac68b3d6bfbbcf38a8
Brandon <brandon2255p@gmail.com>
2019-02-16 19:57:33 +0200

    Added integration test

--- test/fixtures.py
+++ test/fixtures.py
@@ -20,32 +20,31 @@ async def given_a_stream_with_three_events(c, stream_name):
         ],
     )
 
+
 async def given_two_streams_with_two_events(c):
     await c.publish(
         "stream_one",
         [
             messages.NewEvent(
-                "pony_jumped",
+                "pony_splits",
                 data={"Pony": "Derpy Hooves", "Height": 10, "Distance": 13},
             ),
             messages.NewEvent(
-                "pony_jumped",
+                "pony_splits",
                 data={"Pony": "Sparkly Hooves", "Height": 4, "Distance": 9},
             ),
-            
         ],
     )
     await c.publish(
         "stream_two",
         [
             messages.NewEvent(
-                "pony_jumped",
+                "pony_splits",
                 data={"Pony": "Unlikely Hooves", "Height": 63, "Distance": 23},
             ),
             messages.NewEvent(
-                "pony_jumped",
+                "pony_splits",
                 data={"Pony": "Glitter Hooves", "Height": 11, "Distance": 94},
             ),
-            
         ],
     )
--- test/read_test.py
+++ test/read_test.py
@@ -5,7 +5,10 @@ import pytest
 
 from photonpump import connect, exceptions, messages
 
-from .fixtures import given_a_stream_with_three_events
+from .fixtures import (
+    given_a_stream_with_three_events,
+    given_two_streams_with_two_events,
+)
 
 
 @pytest.mark.asyncio
@@ -174,3 +177,37 @@ async def test_iter_from_missing_stream(event_loop):
             assert False
         except Exception as e:
             assert isinstance(e, exceptions.StreamNotFound)
+
+
+@pytest.mark.asyncio
+async def test_iter_all(event_loop):
+
+    async with connect(
+        loop=event_loop, name="iter-all", username="admin", password="changeit"
+    ) as c:
+        await given_two_streams_with_two_events(c)
+
+        result = await c.getAll(max_count=4096)
+
+        assert isinstance(result, list)
+        assert len(result) > 4
+
+        stream_one_events = []
+        stream_two_events = []
+
+        for event in result:
+            if event.stream == "stream_one":
+                stream_one_events.append(event)
+            if event.stream == "stream_two":
+                stream_two_events.append(event)
+
+        assert len(stream_one_events) >= 2
+        assert len(stream_two_events) >= 2
+        event_data = stream_one_events[0].json()
+        assert event_data["Pony"] == "Derpy Hooves"
+        event_data = stream_one_events[1].json()
+        assert event_data["Pony"] == "Sparkly Hooves"
+        event_data = stream_two_events[0].json()
+        assert event_data["Pony"] == "Unlikely Hooves"
+        event_data = stream_two_events[1].json()
+        assert event_data["Pony"] == "Glitter Hooves"
